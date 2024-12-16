d135bb0449b35cc2a4de3921910a18f978e803f4
Ziyi-the-Cheng <ziyi-cheng@outlook.com>
2024-12-16 07:59:15 +0000

    readability improved

--- test/game.cpp
+++ test/game.cpp
@@ -7,20 +7,22 @@
 
 int WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, PSTR lpCmdLine, int nCmdShow) {
 	Window win;
-
+	//create device
 	device d;
+	//create ground
 	Plane p;
+	//create camera
 	Camera cam;
+
+	//create game world objects
 	Sky sky;
-	//Cube c;
-	//Sphere sp;
-	//Tree t;
-	loader lol;
+	loader rock;
 	TRex tr;
 	loader pine;
 	loader pine1;
 	loader pine2;
-	//Sampler samp;
+
+	//create texture managers, textures, collision cubes and light
 	TextureManager mmm;
 	TextureManager pmm;
 	Texture skyt;
@@ -28,30 +30,24 @@ int WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, PSTR lpCmdLine, int nC
 	collisionCube ccPine(Vec3(4.5f, 0, 4.5f), Vec3(5.5f, 10, 5.5f));
 	collisionCube ccPine1(Vec3(4.5f, 0, -10.5f), Vec3(5.5f, 10, -9.5f));
 	collisionCube ccPine2(Vec3(-5.5f, 0, 7.5f), Vec3(-4.5f, 10, 8.5f));
-
-
 	lightSource li;
 
+	//create shaders
 	shader s;
 	shader pp;
 	shader spine;
 	shader ssky;
 	
+	//initiate all components
 	win.create(1024, 1024, "My Window");
 	d.create(1024, 1024, win.hwnd, false);
 	
-	//c.init(d);
 	p.init(d);
 	sky.init(d, 20, 20, 50);
-	//sp.init(d, 19, 19, 2);
-	/*samp.init(d);
-	samp.bind(d);*/
-	//t.init(d);
 	pine.init("pine.gem", d, pmm);
 	pine1.init("pine.gem", d, pmm);
 	pine2.init("pine.gem", d, pmm);
-
-	lol.init("uc1jagbjw.gem", d, pmm);
+	rock.init("uc1jagbjw.gem", d, pmm);
 	spine.create(d, "pvs.txt", "nmPS.txt");
 	s.create(d, "TRexVS.txt", "treePs.txt");
 	pp.create(d, "pvs.txt", "pps.txt");
@@ -66,12 +62,10 @@ int WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, PSTR lpCmdLine, int nC
 
 			win.processMessages();
 			d.clear();
-			//s.apply(d);
-
+			//update camera each frame
 			cam.update(win.mousex, win.mousey, tr.planeWorld);
 
-			collide = tr.collisioncube.isCollide(cc) || tr.collisioncube.isCollide(ccPine) || tr.collisioncube.isCollide(ccPine1) || tr.collisioncube.isCollide(ccPine2);
-
+			//movement control
 			if (win.keyPressed('W')) {
 				cam.moveForward(tr.collisioncube.isCollide(cc) || tr.collisioncube.isCollide(ccPine) || tr.collisioncube.isCollide(ccPine1) || tr.collisioncube.isCollide(ccPine2));
 				tr.move(0.f, 1.f, cc, ccPine, ccPine1, ccPine2);
@@ -85,14 +79,14 @@ int WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, PSTR lpCmdLine, int nC
 
 			tr.move(cam.delta, 0.f, cc, ccPine, ccPine1, ccPine2);
 
-
+			//draw everything
 			sky.draw(&ssky, d, cam, skyt);
 			p.draw(&pp, d, cam);
 			Matrix m;
 			m = m.scale(Vec3(0.5f, 0.5f, 0.5f)) * m.translation(Vec3(15.f, 0, 15.f));
 			
-			lol.updateW(m);
-			lol.draw(&spine, d, pmm, cam, li);
+			rock.updateW(m);
+			rock.draw(&spine, d, pmm, cam, li);
 
 			m = m.scale(Vec3(0.01f, 0.01f, 0.01f)) * m.translation(Vec3(5.f, 0, 5.f));
 			pine.updateW(m);
@@ -104,13 +98,7 @@ int WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, PSTR lpCmdLine, int nC
 			pine2.updateW(m);
 			pine2.draw(&spine, d, pmm, cam, li);
 			tr.draw(&s, d, mmm, cam, moving);
-			/*m = m.translation(Vec3(0, 0, 0));
-			c.updateW(m);
-			c.draw(&s, d);
-			m = m.translation(Vec3(5, 0, 0));
-			c.updateW(m);
-			c.draw(&s, d);*/
-			//sp.draw(&s, d);
+			
 			d.present();
 			
 		}
